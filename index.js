697854eb5524dc4c8409d5d072fb10686acaad4b
Goziem Ajulibe <165010444+GoziemAjulibe@users.noreply.github.com>
2024-08-07 22:21:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
+
+
 document.querySelector(".hamburger-menu").addEventListener("click", function(){
     document.querySelector(".guide-container").classList.remove("closed");
     document.querySelector(".guide-container").classList.add("open");
 })
 
+
 document.querySelector(".close-icon").addEventListener("click", function(){
     document.querySelector(".guide-container").classList.remove("open");
     document.querySelector(".guide-container").classList.add("closed");
 })
+
+
+function closeGuideContainer() {
+    const guideContainer = document.querySelector(".guide-container");
+    if (guideContainer) {
+        guideContainer.classList.remove("open");
+        guideContainer.classList.add("closed");
+    }
+ }
+    
+
+function handleGuideContainer() {
+
+    const guideItems = document.querySelectorAll(".guide-item");
+
+    guideItems.forEach(item => {
+        item.addEventListener("click", function() {
+        closeGuideContainer();
+        });
+
+    });
+
+}
+
+handleGuideContainer();
+
 
 window.addEventListener("scroll", function() {
     const pageNav = document.querySelector("header");
